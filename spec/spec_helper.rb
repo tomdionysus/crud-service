08285235fc29732de90b5cdd2cda42d1331d7d2d
Tom Cully <tom.cully@loyalty.co.nz>
2015-08-26 10:33:01 +1200

    Refactored :stub to :expect

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ Dotenv.load
 module Helpers
   def mysql_mock
     mysql = double("Mysql2")
-    mysql.stub(:escape) do |s|
+    allow(mysql).to receive(:escape) do |s|
       Mysql2::Client.escape(s)
     end
     mysql
@@ -12,23 +12,22 @@ module Helpers
 
   # Mock a Mysql Result object that returns each_hash
   def mysql_result_mock(data)
-    result = double(result)
-    mock = result.stub(:each)
+    result = double("Result")
 
-    data.each do |hash|
-      mock = mock.and_yield(hash)
-    end
+    allow(result).to receive(:each) { |&block|
+      data.each { |item| block.call(item) }
+    }
 
-    result.stub(:count).and_return(data.length)
+    allow(result).to receive(:count).and_return(data.length)
 
     result
   end
 
   # Make the supplied memcache_mock null (no hits, null write)
   def memcache_null(memcache_mock)
-    @mock_memcache.stub(:get).and_return(nil)
-    @mock_memcache.stub(:set).and_return(nil)
-    @mock_memcache.stub(:incr).and_return(nil)
+    allow(@mock_memcache).to receive(:get).and_return(nil)
+    allow(@mock_memcache).to receive(:set).and_return(nil)
+    allow(@mock_memcache).to receive(:incr).and_return(nil)
   end
 end
 
